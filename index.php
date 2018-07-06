3c9709e752cb1025f8da9447de887ea51cb4e7e6
root <root@testapp.example.com>
2018-07-07 04:40:32 +0530

    My first web application push-1

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h2>Test application server!</h2>";
+echo "Hello world!<br>";
+echo "Testing the PHP! configuration<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?> 
+
+</body>
+</html>
