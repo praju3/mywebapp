5427bfaf6f27ab22ab44a0fcd857bec6a88ae19e
root <root@testapp.example.com>
2018-07-07 05:28:50 +0530

    second change

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 echo "<h2>Test application server!</h2>";
 echo "Hello world!<br>";
 echo "Testing the PHP! configuration<br>";
+echo "succeded point<br>";
 echo "This ", "string ", "was ", "made ", "with multiple parameters.";
 ?> 
 
